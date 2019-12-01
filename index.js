b49392e774d869d3ed22921d972636f7bf1b4380
Kristian Lutak <kristian.lutak@student.tuke.sk>
2019-12-01 21:05:23 +0100

    import physi js library

--- index.js
+++ index.js
@@ -1,117 +1,149 @@
-var scene, renderer;
-var camera, cameraControls;
+'use strict';
 
+Physijs.scripts.worker = 'Physijs/physijs_worker.js';
+Physijs.scripts.ammo = 'ammo.js';
+
+var scene, renderer, gui;
+var camera, cameraControls, light;
+var ground, ground_material;
 var pas;
 
 var Variables = function () {
-  this.speed = 1.0;
+    this.speed = 1.0;
 };
 var variables = new Variables();
 
-var gui = new dat.GUI();
+gui = new dat.GUI();
 gui.add(variables, 'speed', 0, 2);
-
 
 if (!init()) animate();
 
 // init the scene
 function init() {
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,	// to get smoother output
+    });
 
-  renderer = new THREE.WebGLRenderer({
-    antialias: true,	// to get smoother output
-  });
+    renderer.setClearColor(0xbbbbbb);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.getElementById('container').appendChild(renderer.domElement);
 
-  renderer.setClearColor(0xbbbbbb);
+    // create a scene
+    scene = new Physijs.Scene;
+    scene.setGravity(new THREE.Vector3(0, -30, 0));
+    scene.addEventListener(
+        'update',
+        function () {
+            scene.simulate(undefined, 1);
+        }
+    );
 
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.getElementById('container').appendChild(renderer.domElement);
+    // put a camera in the scene
+    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.set(0, 0, 5);
+    scene.add(camera);
 
-  // create a scene
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color(0, 0, 0);
+    // lighting
+    light = new THREE.AmbientLight(0x404040, 15); // soft white light
+    scene.add(light);
 
-  // put a camera in the scene
-  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 10000);
-  camera.position.set(0, 0, 5);
-  scene.add(camera);
+    // create a camera control
+    cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
 
-  var light = new THREE.AmbientLight(0x404040, 15); // soft white light
-  scene.add(light);
+    // Materials
+    ground_material = Physijs.createMaterial(
+        new THREE.MeshStandardMaterial({color: 0x747F70, side: THREE.DoubleSide}), 1, .9 // low restitution
+    );
 
-  var geometryPlane = new THREE.PlaneGeometry(10, 10, 4, 4);
-  var materialPlane = new THREE.MeshBasicMaterial({
-    color: 0x747F70,
-    side: THREE.DoubleSide
-  });
-  let plane = new THREE.Mesh(geometryPlane, materialPlane);
-  plane.position.set(0, 0, 0);
-  plane.rotation.x = Math.PI / 2;
-  scene.add(plane);
+    // Ground
+    ground = new Physijs.PlaneMesh(new THREE.BoxGeometry(10, 0.5, 10), ground_material, 0 // mass
+    );
+    ground.receiveShadow = true;
+    ground.position.set(0, -0.5, 0);
+    scene.add(ground);
 
-
-  // create a camera contol
-  cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
-  // Instantiate a loader
-  var loader = new THREE.GLTFLoader();
-  // Load a glTF resource
-  loader.load(
-      // resource URL
-      'js/assets/conveyor/scene.gltf',
-      // called when the resource is loaded
-      function (gltf) {
-        // console.log(gltf)
-        scene.add(gltf.scene.children[0]);
-        findAll();
-      },
-      // called while loading is progressing
-      function (xhr) {
-
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-
-      },
-      // called when loading has errors
-      function (error) {
-        console.log(error);
-        console.log('An error happened');
-
-      }
-  );
-
+    // Instantiate a loader
+    var loader = new THREE.GLTFLoader();
+    loader.load(
+        'js/assets/conveyor/scene.gltf',
+        // called when the resource is loaded
+        function (gltf) {
+            scene.add(gltf.scene.children[0]);
+            // findAll();
+        },
+        // called while loading is progressing
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        // called when loading has errors
+        function (error) {
+            console.log('An error happened');
+        }
+    );
+    setInterval(createShape, 1000);
 }
 
-// animation loop
-function animate() {
-  requestAnimationFrame(animate);
+function createShape() {
+    var box_geometry = new THREE.CubeGeometry(1, 1, 1),
+        sphere_geometry = new THREE.SphereGeometry(1, 5, 5),
+        cylinder_geometry = new THREE.CylinderGeometry(2, 2, 1, 32),
+        cone_geometry = new THREE.CylinderGeometry(0, 2, 4, 32),
+        octahedron_geometry = new THREE.OctahedronGeometry(1.7, 1),
+        torus_geometry = new THREE.TorusKnotGeometry(1.7, .2, 32, 4);
 
-  if (!! pas) {
-    // pas.rotation.x += 0.05;
-    // pas.rotation.y += 0.05;
-    // pas.rotation.z += 0.15;
-  }
-  // do the render
-  render();
+    var shape, material = new THREE.MeshLambertMaterial({opacity: 0, transparent: true});
 
+    switch (Math.floor(Math.random() * 2)) {
+        case 0:
+            shape = new Physijs.BoxMesh(
+                box_geometry,
+                material
+            );
+            break;
+
+        case 1:
+            shape = new Physijs.SphereMesh(
+                sphere_geometry,
+                material,
+                undefined,
+                {restitution: Math.random() * 1.5}
+            );
+            break;
+    }
+
+    shape.material.color.setRGB(Math.random() * 100 / 100, Math.random() * 100 / 100, Math.random() * 100 / 100);
+    shape.castShadow = true;
+    shape.receiveShadow = true;
+
+    shape.position.set(0, 0.5, 0);
+
+    shape.rotation.set(
+        Math.random() * Math.PI,
+        Math.random() * Math.PI,
+        Math.random() * Math.PI
+    );
+    scene.add(shape);
+    console.log(scene);
 }
 
 // render the scene
 function render() {
-
-  // update camera controls
-  cameraControls.update();
-
-  // actually render the scene
-  renderer.render(scene, camera);
+    cameraControls.update();
+    scene.simulate();
+    renderer.render(scene, camera);
 }
 
-
-const findAll = () => {
-
-  let idx = 0;
-  scene.traverse((obj) => {
-
-    if (obj.name === 'Conveyor01Belt_Belt_0') {
-      pas = obj;
-      console.log(pas);
-    }
-  })
+// animation loop
+function animate() {
+    requestAnimationFrame(animate);
+    render();
 }
+
+// const findAll = () => {
+//     scene.traverse((obj) => {
+//         if (obj.name === 'Conveyor01Belt_Belt_0') {
+//             pas = obj;
+//             console.log(pas);
+//         }
+//     })
+// };
